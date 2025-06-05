64d9b1ecae823684a7bc2f65ff4fc62c10e83b01
biboletin <shady_fan@abv.bg>
2025-06-05 23:34:40 +0300

    Extends service provider with more functionalities

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,10 +6,43 @@ use Biboletin\Interfaces\ServiceProviderInterface;
 
 abstract class ServiceProvider implements ServiceProviderInterface
 {
-    
+    protected $container;
+
     abstract public function register(): void;
 
     public function boot(): void
     {
     }
+    
+    public function getContainer()
+    {
+        return $this->container;
+    }
+    
+    public function setContainer($container)
+    {
+        $this->container = $container;
+    }
+    
+    public function get($key)
+    {
+        return $this->container->get($key);
+    }
+    
+    public function set($key, $value)
+    {
+        $this->container->set($key, $value);
+    }
+    
+    public function has($key)
+    {
+        return $this->container->has($key);
+    }
+    
+    public function remove($key)
+    {
+        $this->container->remove($key);
+    }
+    
+    
 }
